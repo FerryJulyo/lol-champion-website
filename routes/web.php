c2d6d742ac5ad36304a93a4f29957d9ad337157a
FerryJulyo <ferryjulyo86@gmail.com>
2025-10-21 14:23:18 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
+use App\Http\Controllers\ChampionController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('home');
-});
+Route::get('/', [ChampionController::class, 'index'])->name('home');
+Route::get('/champions', [ChampionController::class, 'index'])->name('champions.index');
+Route::get('/champions/{championId}', [ChampionController::class, 'show'])->name('champions.show');
+Route::get('/roles', [ChampionController::class, 'roles'])->name('champions.roles');
+Route::get('/difficulty', [ChampionController::class, 'difficulty'])->name('champions.difficulty');
+Route::get('/search', [ChampionController::class, 'search'])->name('champions.search');
+Route::get('/about', function () {
+    return view('about');
+})->name('about');
